e757c2cfdcb0801fffe4aab3d69b50c77425bc75
Krishna-13-cyber <krishnanarayanan132002@gmail.com>
2023-07-17 15:27:03 +0530

    Update with reverting sphinx_rtd_theme used in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,17 +9,17 @@ on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 if on_rtd:
     subprocess.call('cd ..; doxygen', shell=True)
 
-# import sphinx_rtd_theme
+import sphinx_rtd_theme
 
-# html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_rtd_theme"
 
-# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
-# def setup(app):
-#     app.add_stylesheet("main_stylesheet.css")
+def setup(app):
+    app.add_stylesheet("main_stylesheet.css")
 
-# extensions = ['breathe']
-# breathe_projects = { 'xeus-cpp': '../xml' }
+extensions = ['breathe']
+breathe_projects = { 'xeus-cpp': '../xml' }
 templates_path = ['_templates']
 html_static_path = ['_static']
 source_suffix = '.rst'
