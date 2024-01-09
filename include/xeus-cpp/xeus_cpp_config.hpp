15d4e999e9444a846ae88ba7ee4542075b2093e7
Johan Mabille <johan.mabille@gmail.com>
2024-01-09 09:38:03 +0100

    Release 0.3.0

--- include/xeus-cpp/xeus_cpp_config.hpp
+++ include/xeus-cpp/xeus_cpp_config.hpp
@@ -11,7 +11,7 @@
 
 // Project version
 #define XEUS_CPP_VERSION_MAJOR 0
-#define XEUS_CPP_VERSION_MINOR 2
+#define XEUS_CPP_VERSION_MINOR 3
 #define XEUS_CPP_VERSION_PATCH 0
 
 // Composing the version string from major, minor and patch
