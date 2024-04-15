b2ea844e8750a4ab9ae9bd95ee996b7ecddbc3f4
Alexander Penev <alexander.p.penev@gmail.com>
2024-04-15 13:27:58 +0000

    Add more tests

--- test/test_xcpp_kernel.py
+++ test/test_xcpp_kernel.py
@@ -18,13 +18,16 @@ class XCppTests(jupyter_kernel_test.KernelTests):
     language_name = 'C++'
 
     # Code that should write the exact string `hello, world` to STDOUT
-    code_hello_world = '#include <iostream>\nstd::cout << "hello, world" << std::endl;'
+    code_hello_world = '#include <iostream>\n#include <stdio.h>\nstd::cout << "hello, world" << std::endl;\nprintf("Hello from printf%s", "!");'
 
     # Code that should cause (any) text to be written to STDERR
     code_stderr = '#include <iostream>\nstd::cerr << "oops" << std::endl;'
 
     # Pager: code that should display something (anything) in the pager
-    #code_page_something = "?std::vector"
+    code_page_something = "?std::vector"
+
+    # Exception throwing
+    code_generate_error = 'throw std::runtime_error("Unknown exception");'
 
     # Samples of code which generate a result value (ie, some text
     # displayed as Out[n])
@@ -41,6 +44,31 @@ class XCppTests(jupyter_kernel_test.KernelTests):
         {
             'code': '#include <string>\n#include "xcpp/xdisplay.hpp"\nstd::string test("foobar");\nxcpp::display(test);',
             'mime': 'text/plain'
+        },
+        {
+            'code': """
+#include <string>
+#include <fstream>
+#include "nlohmann/json.hpp"
+#include "xtl/xbase64.hpp"
+namespace im {
+  struct image {
+    inline image(const std::string& filename) {
+      std::ifstream fin(filename, std::ios::binary);
+      m_buffer << fin.rdbuf();
+    }
+    std::stringstream m_buffer;
+  };
+  nlohmann::json mime_bundle_repr(const image& i) {
+    auto bundle = nlohmann::json::object();
+    bundle["image/png"] = xtl::base64encode(i.m_buffer.str());
+    return bundle;
+  }
+}
+#include "xcpp/xdisplay.hpp"
+im::image marie("../notebooks/images/marie.png");
+xcpp::display(marie);""",
+            'mime': 'image/png'
         }
     ]
 
