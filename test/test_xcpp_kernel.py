12046fa0c8b358993927f29004413393c3383aba
mcbarton <150042563+mcbarton@users.noreply.github.com>
2024-05-13 21:25:21 +0100

    Update test_xcpp_kernel.py

--- test/test_xcpp_kernel.py
+++ test/test_xcpp_kernel.py
@@ -140,10 +140,10 @@ class XCppTests4(jupyter_kernel_test.KernelTests):
     language_name = 'C++'
 
     # Code that should write the exact string `hello, world` to STDOUT
-    code_hello_world = '#include <iostream>\n std::cout << "hello, world" << std::endl;'
+    code_hello_world = '#include <iostream>\n std::cout << "hello, world" ;'
 
     # Code that should cause (any) text to be written to STDERR
-    code_stderr = '#include <iostream>\n std::cerr << "oops" << std::endl;'
+    code_stderr = '#include <iostream>\n std::cerr << "oops" ;'
 
     # Pager: code that should display something (anything) in the pager
     code_page_something = "?std::vector"
