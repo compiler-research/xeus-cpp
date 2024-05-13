5fe1df326ea3d67cd63518f08dadd2671a243e58
mcbarton <150042563+mcbarton@users.noreply.github.com>
2024-05-13 18:49:23 +0100

    Try to fix test for Windows (expected to fail Unix)

--- test/test_xcpp_kernel.py
+++ test/test_xcpp_kernel.py
@@ -137,15 +137,7 @@ class XCppTests2(jupyter_kernel_test.KernelTests):
     language_name = 'C++'
 
     # Code that should write the exact string `hello, world` to STDOUT
-
-
-    #ifdef _WIN32
-      code_hello_world = '#include <stdio.h>\r\nprintf("hello, world");'
-    #endif
-    
-    #ifndef _WIN32
-      code_hello_world = '#include <stdio.h>\nprintf("hello, world");'
-    #endif
+    code_hello_world = '#include <stdio.h>\r\nprintf("hello, world");'
 
 if __name__ == '__main__':
     unittest.main()
