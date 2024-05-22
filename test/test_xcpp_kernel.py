93aa2e48a033d7eeae25802dffd9d34542fc89d4
mcbarton <150042563+mcbarton@users.noreply.github.com>
2024-05-22 17:58:53 +0100

    Go back to just headers needed for test

--- test/test_xcpp_kernel.py
+++ test/test_xcpp_kernel.py
@@ -135,16 +135,6 @@ if platform.system() != 'Windows':
     #include <omp.h>
     #include <iostream>
     #include <clang/Interpreter/CppInterOp.h>  
-    #include "llvm/Support/FileSystem.h"
-    #include "llvm/Support/Path.h"
-    llvm::StringRef Dir = llvm::sys::path::parent_path("/home/runner/micromamba/envs/xeus-cpp/");
-    Cpp::AddSearchPath(Dir.str().c_str());
-    #ifdef __APPLE__
-      std::string PathToTestSharedLib = Cpp::SearchLibrariesForSymbol("_omp_get_max_threads_", /*system_search=*/false);
-    #else
-      std::string PathToTestSharedLib = Cpp::SearchLibrariesForSymbol("omp_get_max_threads_", /*system_search=*/false);
-    #endif // __APPLE__
-    std::cerr<<Dir.str().c_str()<<<<std::endl;
     """
         def test_xcpp_omp(self):
             self.flush_channels()
