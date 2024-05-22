9800c8ac08acb01dd2e6d26961c2713fc36ad750
mcbarton <150042563+mcbarton@users.noreply.github.com>
2024-05-22 15:47:52 +0100

    Add more code back to test

--- test/test_xcpp_kernel.py
+++ test/test_xcpp_kernel.py
@@ -137,6 +137,9 @@ if platform.system() != 'Windows':
     #include <clang/Interpreter/CppInterOp.h>  
     #include "llvm/Support/FileSystem.h"
     #include "llvm/Support/Path.h"
+    std::string BinaryPath = GetExecutablePath(/*Argv0=*/nullptr);
+    llvm::StringRef Dir = llvm::sys::path::parent_path(BinaryPath);
+    Cpp::AddSearchPath(Dir.str().c_str());
     """
         def test_xcpp_omp(self):
             self.flush_channels()
