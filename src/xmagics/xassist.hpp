bc36b1496897d4bf1597f2f8bfdcedca6733dcdc
tharun571 <atharun05@gmail.com>
2024-06-24 18:06:04 +0530

    xassist implementation

---
+++ src/xmagics/xassist.hpp
@@ -0,0 +1,25 @@
+/************************************************************************************
+ * Copyright (c) 2023, xeus-cpp contributors                                        *
+ *                                                                                  *
+ * Distributed under the terms of the BSD 3-Clause License.                         *
+ *                                                                                  *
+ * The full license is in the file LICENSE, distributed with this software.         *
+ ************************************************************************************/
+
+#ifndef XEUS_CPP_XASSIST_MAGIC_HPP
+#define XEUS_CPP_XASSIST_MAGIC_HPP
+
+#include <string>
+
+#include "xeus-cpp/xmagics.hpp"
+
+namespace xcpp
+{
+    class xassist : public xmagic_cell
+    {
+    public:
+
+        void operator()(const std::string& line, const std::string& cell) override;
+    };
+}  // namespace xcpp
+#endif
