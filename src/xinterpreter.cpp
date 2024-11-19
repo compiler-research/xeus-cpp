b75cb90b6d1a32f3896aea7522e11dddc8f8cf1e
Anutosh Bhat <andersonbhat491@gmail.com>
2024-11-19 21:54:58 +0530

    refactor: remove unused #include directives

--- src/xinterpreter.cpp
+++ src/xinterpreter.cpp
@@ -7,17 +7,8 @@
  * The full license is in the file LICENSE, distributed with this software.         *
  ************************************************************************************/
 
-#include <algorithm>
-#include <cinttypes>  // required before including llvm/ExecutionEngine/Orc/LLJIT.h because missing llvm/Object/SymbolicFile.h
-#include <cstdarg>
-#include <cstdio>
-#include <memory>
-#include <sstream>
-#include <string>
-#include <vector>
-
 #include "xeus/xsystem.hpp"
-#include <xeus/xhelper.hpp>
+#include "xeus/xhelper.hpp"
 
 #include "xeus-cpp/xbuffer.hpp"
 #include "xeus-cpp/xeus_cpp_config.hpp"
