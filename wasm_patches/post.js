7f75a4280bd08257fcacc59b68705787e0ce15c4
mcbarton <matthew.c.barton@hotmail.co.uk>
2025-06-06 16:33:26 +0100

    Remove unused getDylinkMetadata in wasm_patches

--- wasm_patches/post.js
+++ wasm_patches/post.js
@@ -5,7 +5,6 @@ if (!('wasmTable' in Module)) {
 Module.FS = FS
 Module.PATH = PATH
 Module.LDSO = LDSO
-Module.getDylinkMetadata = getDylinkMetadata
 Module.loadDynamicLibrary = loadDynamicLibrary
 
 Module.UTF8ToString = UTF8ToString;
