3d2e16febd99f61998700ca6ca5776b8ffa20a26
mcbarton <matthew.c.barton@hotmail.co.uk>
2025-07-11 16:02:14 +0100

    Improve doctest output (#351)

--- test/main.cpp
+++ test/main.cpp
@@ -6,5 +6,20 @@
  * The full license is in the file LICENSE, distributed with this software.
  ****************************************************************************/
 
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#define DOCTEST_CONFIG_IMPLEMENT
 #include "doctest/doctest.h"
+
+int main(int argc, char** argv) {
+    doctest::Context context;
+
+    // Set options to show more detailed test output
+    context.setOption("success", true);
+    context.setOption("report-failures", true);
+    context.setOption("force-colors", true);
+    
+    context.applyCommandLine(argc, argv);
+
+    int res = context.run();
+
+    return res;
+}
