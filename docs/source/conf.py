4ae19d323d945ac7c576033f4c2fa37e7698e6f2
anutosh491 <andersonbhat491@gmail.com>
2025-11-27 10:49:12 +0530

    Use jupyterlite-xeus 4.3.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,9 +34,8 @@ emcmake cmake -DCMAKE_BUILD_TYPE=Release \\
               {XEUS_CPP_ROOT};
 emmake make -j $(nproc --all) install;
 cd {XEUS_CPP_ROOT};
-micromamba create -n xeus-lite-host jupyterlite-core=0.6 jupyter_server jupyterlite-xeus -c conda-forge -y;
+micromamba create -n xeus-lite-host jupyter_server jupyterlite-xeus -c conda-forge -y;
 micromamba activate xeus-lite-host;
-python -m pip install jupyterlite-xeus jupyter_server;
 jupyter lite build --XeusAddon.prefix=$PREFIX \\
                    --XeusAddon.mounts="$PREFIX/share/xeus-cpp/tagfiles:/share/xeus-cpp/tagfiles" \
                    --XeusAddon.mounts="$PREFIX/etc/xeus-cpp/tags.d:/etc/xeus-cpp/tags.d" \
