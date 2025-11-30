e8d8ed80f672de39b9a58708da4d77deab8ac5bc
mcbarton <150042563+mcbarton@users.noreply.github.com>
2025-11-30 18:07:27 +0000

    Adjust timeout and apply ruff fix

--- scripts/automated-notebook-run-script.py
+++ scripts/automated-notebook-run-script.py
@@ -8,7 +8,6 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
-import platform
 import sys
 
 
