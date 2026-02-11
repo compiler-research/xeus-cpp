9a2ea92daf6c81f261c7cb681fa7062641d3149f
mcbarton <matthew.c.barton@hotmail.co.uk>
2026-02-11 10:27:51 +0000

    Try Vipul Valgrind issue

--- src/xinterpreter.cpp
+++ src/xinterpreter.cpp
@@ -31,9 +31,10 @@ using Args = std::vector<const char*>;
 
 void* createInterpreter(const Args &ExtraArgs = {}) {
   Args ClangArgs = {/*"-xc++"*/"-v"};
+  std::string resource_dir;
   if (std::find_if(ExtraArgs.begin(), ExtraArgs.end(), [](const std::string& s) {
     return s == "-resource-dir";}) == ExtraArgs.end()) {
-    std::string resource_dir = Cpp::DetectResourceDir();
+    resource_dir = Cpp::DetectResourceDir();
     if (!resource_dir.empty()) {
         ClangArgs.push_back("-resource-dir");
         ClangArgs.push_back(resource_dir.c_str());
